c1af7e1ad7bdb40e5542f0455eee730a3716032e
Krishna Dhakal <7krishna7dhakal7@gmail.com>
2023-10-03 13:44:43 +0545

    bootcamp route

---
+++ src/app/bootcamps/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function BootcampLists() {
+	return (
+		<>
+		<div>List of Bootcamps</div>
+		<div>2023</div>
+		<div>2024</div>
+		</>
+	)
+}
+
+export default BootcampLists
